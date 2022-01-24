bf7c229368d7bdfdd27fbf826625841f9e77fa80
Patrick Ferris <patrick@sirref.org>
2022-01-24 22:22:05 +0000

    Initial partial bindings to autocomplete

---
+++ src/autocomplete/autocomplete.ml
@@ -0,0 +1,89 @@
+open Code_mirror
+
+module RegExp = RegExp
+let autocomplete = Jv.get Jv.global "__CM__autocomplete"
+
+module Completion = struct
+  type t = Jv.t
+
+  include (Jv.Id : Jv.CONV with type t := t)
+
+  let label t = Jv.Jstr.get t "label" |> Jstr.to_string
+  let detail t = Jv.Jstr.find t "detail" |> Option.map Jstr.to_string
+end
+
+type config = Jv.t
+
+let config 
+  ?activate_on_typing
+  ?override
+  ?max_rendered_options
+  ?default_key_map
+  ?above_cursor
+  ?option_class
+  ?icons
+  ?add_to_options
+  () = 
+  let o = Jv.obj [||] in
+   Jv.Bool.set_if_some o "activateOnTyping" activate_on_typing;
+   Jv.set_if_some o "override" override;
+   Jv.Int.set_if_some o "maxRenderedOptions" max_rendered_options;
+   Jv.Bool.set_if_some o "defaultKeyMap" default_key_map;
+   Jv.Bool.set_if_some o "aboveCursor" above_cursor;
+   Jv.set_if_some o "optionClass" option_class;
+   Jv.Bool.set_if_some o "icons" icons;
+   Jv.set_if_some o "addToOptions" add_to_options;
+   o
+
+let create ?(config = Jv.null) () =
+  Extension.of_jv @@
+  Jv.call autocomplete "autocompletion" [| config |]
+
+(* type status = Active | Pending
+
+let status state = 
+
+val status : Editor.State.t -> status option
+(** Gets the current completion status *)
+
+val current_completions : Editor.State.t -> Completion.t list
+(** Returns the current available completions *)
+
+val selected_completion : Editor.State.t -> Completion.t option
+* Returh the currently selected completion if any *)
+
+
+module Context = struct
+  type t = Jv.t
+  (** Completion context *)
+
+  include (Jv.Id : Jv.CONV with type t := t)
+
+  let token_before t types =
+    let jv = Jv.call t "tokenBefore" [| Jv.of_list Jv.of_string types |] in
+    if Jv.is_null jv then None else Some jv
+
+  let match_before t regex =
+    let jv = Jv.call t "matchBefore" [| RegExp.to_jv regex |] in
+    if Jv.is_null jv then None else Some jv
+end
+
+module Result = struct
+  type t = Jv.t
+  (** Completion result *)
+
+  include (Jv.Id : Jv.CONV with type t := t)
+
+  let create ~from ?to_ ~options ?span ?filter () =
+    let o = Jv.obj [||] in
+    Jv.Int.set o "from" from;
+    Jv.Int.set_if_some o "to" to_;
+    Jv.set o "options" (Jv.of_list Completion.to_jv options);
+    Jv.set_if_some o "span" (Option.map RegExp.to_jv span);
+    Jv.Bool.set_if_some o "filter" filter;
+    o
+end
+
+type source = Context.t -> Result.t option Fut.t
+(** A completion source *)
+
---
+++ src/autocomplete/autocomplete.mli
@@ -0,0 +1,63 @@
+val autocomplete : Jv.t
+(** Global autocomplete value *)
+
+module RegExp = RegExp
+
+module Completion : sig
+  type t
+
+  include Jv.CONV with type t := t
+
+  val label : t -> string
+  val detail : t -> string option
+end
+
+type config
+
+val config : 
+  ?activate_on_typing:bool -> 
+  ?override:Jv.t ->
+  ?max_rendered_options:int ->
+  ?default_key_map:bool ->
+  ?above_cursor:bool ->
+  ?option_class:Jv.t ->
+  ?icons:bool ->
+  ?add_to_options:Jv.t ->
+  unit ->
+  config
+  (** Configuration options for your autocompleter, see {{: https://codemirror.net/6/docs/ref/#autocomplete.autocompletion^config} the online docs}.*)
+
+val create :
+  ?config:config -> unit ->
+  Code_mirror.Extension.t
+  (** Autocompleter *)
+
+module Context : sig
+  type t
+  (** Completion context *)
+
+  include Jv.CONV with type t := t
+
+  val token_before : t -> string list -> Jv.t option
+
+  val match_before : t -> RegExp.t -> Jv.t option
+end
+
+module Result : sig
+  type t
+  (** Completion result *)
+
+  include Jv.CONV with type t := t
+
+  val create :
+    from:int ->
+    ?to_:int ->
+    options:Completion.t list ->
+    ?span:RegExp.t ->
+    ?filter:bool ->
+    unit -> t
+  (** Creating a new completion result (see {{: https://codemirror.net/6/docs/ref/#autocomplete.CompletionResult} the docs}).*)
+end
+
+type source = Context.t -> Result.t option Fut.t
+(** A completion source *)
