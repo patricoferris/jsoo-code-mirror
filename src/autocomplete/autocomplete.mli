f31996cbb871c3080529879af8f7c11bd7ba3ed1
Ulysse Gérard <thevoodoos@gmail.com>
2022-04-12 10:12:51 -0300

    Autocomplete: more complete

--- src/autocomplete/autocomplete.mli
+++ src/autocomplete/autocomplete.mli
@@ -1,3 +1,5 @@
+open Code_mirror
+
 val autocomplete : Jv.t
 (** Global autocomplete value *)
 
@@ -24,9 +26,25 @@ module Context : sig
 
   include Jv.CONV with type t := t
 
+  val state : t -> Editor.State.t
+  (** The editor state that the completion happens in. *)
+
+  val pos : t -> int
+  (** The position at which the completion is happening. *)
+
+  val explicit : t -> bool
+  (** Indicates whether completion was activated explicitly, or implicitly by
+    typing. The usual way to respond to this is to only return completions when
+    either there is part of a completable entity before the cursor, or explicit
+    is true. *)
+
   val token_before : t -> string list -> Jv.t option
 
   val match_before : t -> RegExp.t -> Jv.t option
+
+  val aborted : t -> bool
+  (** Yields true when the query has been aborted. Can be useful in
+    asynchronous queries to avoid doing work that will be ignored. *)
 end
 
 module Result : sig
@@ -45,15 +63,22 @@ module Result : sig
   (** Creating a new completion result (see {{: https://codemirror.net/6/docs/ref/#autocomplete.CompletionResult} the docs}).*)
 end
 
-type source = Context.t -> Result.t option Fut.t
-(** A completion source *)
+module Source : sig
+  type t
+  (** A Completion source *)
 
+  include Jv.CONV with type t := t
+
+  val create : (Context.t -> Result.t option Fut.t) -> t
+
+  val from_list : Completion.t list -> t
+end
 
 type config
 
-val config : 
-  ?activate_on_typing:bool -> 
-  ?override:source list ->
+val config :
+  ?activate_on_typing:bool ->
+  ?override:Source.t list ->
   ?max_rendered_options:int ->
   ?default_key_map:bool ->
   ?above_cursor:bool ->
